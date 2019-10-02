ffc16f20e7477321e0e0a9bd4de33c151f2abd46
didal <didal@udm.ru>
2019-10-02 22:44:52 +0400

    Рисуем древко

---
+++ flag.py
@@ -0,0 +1,20 @@
+import pygame
+
+# инициализация Pygame:
+pygame.init()
+# размеры окна:
+size = width, height = 800, 600
+# screen — холст, на котором нужно рисовать:
+screen = pygame.display.set_mode(size)
+screen.fill((255, 255, 255))
+# формирование кадра:
+# команды рисования на холсте
+# Рисуем древко
+screen.fill(pygame.Color('brown'), pygame.Rect(10, 10, 15, 580))
+# смена (отрисовка) кадра:
+pygame.display.flip()
+# ожидание закрытия окна:
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+# завершение работы:
+pygame.quit()
